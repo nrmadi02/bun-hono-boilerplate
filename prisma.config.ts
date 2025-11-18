9b0eeaef815cfa5f377141e9f046ad3ebe3b8766
nrmadi02 <ahmadinur2202@gmail.com>
2025-11-18 14:54:54 +0800

    chore: update config files

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,9 @@
 import "dotenv/config";
 import path from "node:path";
-import type { PrismaConfig } from "prisma";
 
 export default {
-  schema: path.join("prisma", "schema"),
-  migrations: {
-    path: path.join("prisma", "migrations"),
-  },
-} satisfies PrismaConfig;
+	schema: path.join("prisma", "schema"),
+	migrations: {
+		path: path.join("prisma", "migrations"),
+	},
+};

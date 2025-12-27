10a02ca85016cd30b588584fc7c24147e901e997
nrmadi02 <ahmadinur2202@gmail.com>
2025-12-27 21:17:26 +0800

    feat(users): refine users service logic

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,5 +1,10 @@
 import prisma from "prisma";
 
+export interface IListUserParams {
+	limit: number;
+	page: number;
+}
+
 export const findUserById = async (id: string) => {
 	return prisma.user.findUnique({
 		where: { id },
@@ -10,5 +15,14 @@ export const updateUserRole = async (id: string, role: string) => {
 	return prisma.user.update({
 		where: { id },
 		data: { role },
+	});
+};
+
+export const getListUser = async (params: IListUserParams) => {
+	const { limit, page } = params;
+	return prisma.user.paginate().withPages({
+		limit,
+		page,
+		includePageCount: true,
 	});
 };
